ce4cb720d80e05a00311b189aa9ef7de76f2ed71
Kelsey Hawley <kelseyr.hawley@gmail.com>
2015-10-21 19:46:27 -0700

    UI updates, adding ascii checking, error handling

--- a3-encrypted-messaging/gui.py
+++ a3-encrypted-messaging/gui.py
@@ -16,6 +16,10 @@ class Gui:
     LOG_FILE_NAME = "log.txt"
 
     PROGRAM_NAME = "Hi Ildar =). Happy Hacking!"
+
+    #Window Params
+    WIDTH = 400
+    HEIGHT = 490
     
     #Initial Screen 
     CHOICE_LBL = "Choose the option you would like to run the code under: "
@@ -38,10 +42,12 @@ class Gui:
     OK_BTN = "OK"
 
     #Messaging Screen
-    SENT_MESSAGE_DEFAULT = "You have not received any messages yet."
+    SENT_MESSAGE_DEFAULT = "Begin: You have not received any messages yet. The textbox grows upward."
     MSG_INSPECTOR_DEFAULT = "You may expand a messages content here."
+    CURRENT_USER_LBL = "Me"
+    REMOTE_USER_LBL = "Remote User"
     SEND_BTN_TXT = "Send"
-    POLL_DELAY_MS = 150
+    POLL_DELAY_MS = 150 # Interval for checking for new received messaged
 
 
     def __init__(self):
@@ -84,8 +90,9 @@ class Gui:
         #Initialise the window
         self.root = Tk()
         self.root.title(self.PROGRAM_NAME)
-        self.root.minsize(width=600, height=600)
-        self.root.maxsize(width=600, height=600)
+        self.root.minsize(width=self.WIDTH, height=self.HEIGHT)
+        self.root.maxsize(width=self.WIDTH, height=self.HEIGHT)
+        self.root.configure(background='black')
 
         #Initialize the root Frame
         mainframe = ttk.Frame(self.root, padding="3 3 12 12")
@@ -113,17 +120,16 @@ class Gui:
     """
     def initWelcomeScreenGUI(self, parentFrame):
         a = ttk.Label(parentFrame, text=self.errorMessage + "\n" + self.CHOICE_LBL)
-        a.grid(column=2, row=1, sticky=W)
+        a.grid(column=0, row=0, columnspan=3, sticky=W)
         self.widgets.append(a)
 
         b = ttk.Button(parentFrame, text=self.SERVER_BTN, command= lambda: self.initServerGUI(parentFrame))
-        b.grid(column=1, row=5, sticky=W)
+        b.grid(column=0, row=5, sticky=W)
         self.widgets.append(b)
 
         c = ttk.Button(parentFrame, text=self.CLIENT_BTN, command= lambda: self.initClientGUI(parentFrame))
-        c.grid(column=3, row=5, sticky=W)
+        c.grid(column=2, row=5, sticky=E)
         self.widgets.append(c)
-        #self.pause()
 
     """
     Add necessary UI elements for the client configuration options. 
@@ -201,6 +207,33 @@ class Gui:
         self.port = self.port.get()
         self.masterKey = self.masterKey.get()
 
+        try:
+            self.ip.encode('ascii')
+        except UnicodeEncodeError:
+            self.errorMessage = "Whoops! That IP wasn't ascii. We're not sure what you're up to, but we don't like it :) #sorrynotsorry\n"
+            self.showErrorMessage("IP not ASCII", self.errorMessage)
+            self.clearScreen()
+            self.initWelcomeScreenGUI()
+            return
+
+        try:
+            self.port.encode('ascii')
+        except UnicodeEncodeError:
+            self.errorMessage = "Whoops! That port wasn't ascii. We're not sure what you're up to, but we don't like it :) #sorrynotsorry\n"
+            self.showErrorMessage("Port not ASCII", self.errorMessage)
+            self.clearScreen()
+            self.initWelcomeScreenGUI()
+            return
+
+        try:
+            self.masterKey.encode('ascii')
+        except UnicodeEncodeError:
+            self.errorMessage = "Whoops! That key wasn't ascii. We're not sure what you're up to, but we don't like it :) #sorrynotsorry\n"
+            self.showErrorMessage("Key not ASCII", self.errorMessage)
+            self.clearScreen()
+            self.initWelcomeScreenGUI()
+            return
+
         # If in server mode, the ip is irrelevant
         if self.ip in ["", '']:
             self.ip = None
@@ -222,19 +255,14 @@ class Gui:
 
         self.receivedMessageTextWidget = Text(parentFrame, width=41, height=20)
         self.receivedMessageTextWidget.insert('1.0', self.SENT_MESSAGE_DEFAULT)
-        self.receivedMessageTextWidget.grid(column=0, row=0, sticky=(N, W, E, S), padx=2, pady=2)
+        self.receivedMessageTextWidget.grid(column=0, row=0, sticky=(N, W, E, S), padx=2, pady=2, columnspan=2)
         self.receivedMessageTextWidget.config(state=DISABLED) # Read-only
 
         self.sentMessageTextWidget = Text(parentFrame, width=41, height=10)
         self.sentMessageTextWidget.grid(column=0, row=1, sticky=(N, W, E, S), padx=2, pady=2)
-
-        self.messageInspectorTextWidget = Text(parentFrame, width=41, height=30)
-        self.messageInspectorTextWidget.insert('1.0', self.MSG_INSPECTOR_DEFAULT)
-        self.messageInspectorTextWidget.grid(column=7, row=0, sticky=(N, W, E, S), padx=2, pady=2)
-        self.messageInspectorTextWidget.config(state=DISABLED) # Read-only
         
         self.sendBtn = ttk.Button(parentFrame, text=self.SEND_BTN_TXT, command=self.sendMessage)
-        self.sendBtn.grid(column=7, row=1, padx=10, pady=10)
+        self.sendBtn.grid(column=1, row=1, padx=10, pady=10)
 
 
     """
@@ -250,7 +278,7 @@ class Gui:
         if not newMsg == "":
 
             # Add the message to the text widget to view
-            self.postMessage("Remote User: ", str(newMsg))
+            self.postMessage(self.REMOTE_USER_LBL, str(newMsg))
             
         # Schedule this to be called again 
         self.root.after(self.POLL_DELAY_MS, self.checkReceivedMessageQueue)
@@ -270,10 +298,18 @@ class Gui:
     def sendMessage(self):
         # Get the text from the widget
         currentMsg = self.sentMessageTextWidget.get('1.0', 'end')
+
+        try:
+            currentMsg.encode('ascii')
+        except UnicodeEncodeError:
+            self.errorMessage = "Whoops! That wasn't ascii. Try again. #sorrynotsorry\n"
+            self.showErrorMessage("Not ASCII >:(", self.errorMessage)
+            return
+
         # Clear the text box so the next message may be sent
         self.sentMessageTextWidget.delete('1.0', 'end')
         # Add the message to the received text widget for historical purposes
-        self.postMessage("Me", currentMsg)
+        self.postMessage(self.CURRENT_USER_LBL, currentMsg)
         # Send the message through the session manager
         self.session.send(currentMsg)
 
@@ -291,10 +327,15 @@ class Gui:
         self.receivedMessageTextWidget.config(state=DISABLED) #make widget read only again
 
     """
-
     """
     def pause(self, msg):
         messagebox.showinfo("Ready to send?", "Press OK when you're ready to continue.\n\nMSG: " + str(msg))
+
+
+    """
+    """
+    def showErrorMessage(self, title, msg):
+        messagebox.showerror(str(title), str(msg))
 
     """
     Remove all current widgets in the GUI in order to update the contents of the screen.
